1468381f3d039a40e4caac0f9d3dff26fda32abe
Ewan Purves <ewan.purves@gmail.com>
2024-07-29 10:35:27 +0100

    added multiple ingredient input

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -11,8 +11,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   // useState hooks to manage the ingredient and error state. Defined here as states are set and used in different children components from here
-  const [ingredient, setIngredient] = useState("");
+  const [ingredients, setIngredients] = useState([]);
   const [error, setError] = useState("");
+
+  //add an ingredient to the ingredients array
+  const addIngredient = (ingredient) => {
+    //copy the current ingredients array
+    let ing_copy = ingredients.slice();
+    ing_copy.push(ingredient);
+    setIngredients(ing_copy);
+  }
+
+  //remove an ingredient from the ingredients aray
+  const removeIngredient = (ingredient) => {
+    //copy the current ingredients array
+    let ing_copy = ingredients.slice();
+    let index = ing_copy.indexOf(ingredient);
+    if(index != -1) { //check that it exists in the array
+      ing_copy.splice(index,1);
+      setIngredients(ing_copy);
+    }
+  }
 
 
   return (
@@ -20,12 +39,12 @@ function App() {
       <Header logged_in={false} />
       <div className="container mt-5">
         {/* Input component, passing setIngredient function as a prop so we can set the Ingredient state in the Input component */}
-        <Input setIngredient={setIngredient} />
+        <Input addIngredient={addIngredient} ingredients={ingredients} removeIngredient={removeIngredient} />
         {/* Display error message if there's an error */}
         {error && <div className="alert alert-danger">{error}</div>}
         <Switch>
           {/* Route for the recipes page, passing ingredient (set in Ingredient component) and setError (to set state of possible error messages) as props to Recipes component */}
-          <Route path="/" element={<Recipes ingredient={ingredient} setError={setError} />} />
+          <Route path="/" element={<Recipes ingredients={ingredients} setError={setError} />} />
           {/* Route for recipe details, matching any URL with /recipe/:id pattern. This is the structure used by navigate in Recipes.jsx and useParams() gets the id that way */}
           <Route path="/recipe/:id" element={<RecipeDetail />} />
         </Switch>
--- site/src/components/Input.jsx
+++ site/src/components/Input.jsx
@@ -1,30 +1,47 @@
-import React, { useState } from 'react';
+import React, { createRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import IngredientContainer from './IngredientContainer';
 
-const Input = ({ setIngredient }) => { // callback function provided by parent (App) which passes the data (ingredient) back up
+const Input = ({ addIngredient, ingredients, removeIngredient }) => { // callback function provided by parent (App) which passes the data (ingredient) back up
 
     const [inputValue, setInputValue] = useState(""); // state for storing the value (ingredient) in the input field
     const navigate = useNavigate(); // Hook for navigation
 
+    const input_ref = createRef();
+
+
     // Function to handle form submission
     const handleSubmit = (event) => {
         event.preventDefault(); // Prevents default form submission
-        setIngredient(inputValue); // Calls the parent component's setIngredient function
+        addIngredient(inputValue); // Calls the parent component's setIngredient function
         navigate('/'); // Navigates back to the / path which in this case renders Recipe.jsx
+
+        //clear the input field and focus it
+        input_ref.current.focus();
+        setInputValue("");
     };
 
     return (
-        <form className="d-flex mb-4" onSubmit={handleSubmit}>
-            <input
-                type="text"
-                className="form-control me-2"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-                placeholder="Enter an ingredient"
-            />
-            <button type="submit" className="btn btn-primary">Search</button>
-        </form>
+        <div className='input'>
+            <form className="d-flex mb-4" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    autoFocus
+                    ref={input_ref}
+                    className="form-control me-2"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    placeholder="Enter an ingredient"
+                />
+                <button type="submit" className="btn btn-primary">Add Ingredient</button>
+            </form>
+            <div className='ingredients'>
+            {ingredients.map(ingredient => (
+                <IngredientContainer removeIngredient={removeIngredient} ingredient={ingredient}/>
+            ))}
+            </div>
+        </div>
     );
 };
 
