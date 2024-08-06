ada153595200f6039848cb5ca625ed50ff5ce1c4
Ewan Purves <ewan.purves@gmail.com>
2024-08-06 15:23:17 +0100

    added more initial files

--- server/src/main/java/com/sky/server/repos/UserRepo.java
+++ server/src/main/java/com/sky/server/repos/UserRepo.java
@@ -1,7 +1,10 @@
 package com.sky.server.repos;
 
+
+import com.sky.server.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepo {
+public interface UserRepo extends JpaRepository<User, Integer> {
 }
