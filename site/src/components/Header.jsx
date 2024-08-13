ebb6478f6f0d5d942156b021abbe02adcfaed717
Fiona <fiona.beveridge@sky.uk>
2024-08-13 14:38:08 +0100

    profile dropdown added

--- site/src/components/Header.jsx
+++ site/src/components/Header.jsx
@@ -5,6 +5,8 @@ import heart from '../images/bookmark-heart-fill.svg'
 import eats from '../images/eats_white.png';
 import '../css/Header.css';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 //returns a header object with a navbar
 //TODO: pages links need to be added
@@ -23,14 +25,23 @@ const Header = ({logged_in}) => {
                     </div>
                     <div class="col">
                         {/* TODO: Link this to either a login page or a profile page, depending on logged in status */}
-                        <Link to='/login' className='header-right header-link'>
-                            <img src={avatar} alt='profile' className='img-link profile'></img>
-                        </Link>
-                   
+                        <div class="dropdown">
+                            <button class="btn header-right header-link" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src={avatar} alt='profile' className='img-link profile'></img>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><Link to='/login' class="dropdown-item">Login</Link></li>
+                                <li><Link to='/profile' class="dropdown-item">Profile</Link></li>
+                                <li><Link to='/logout' class="dropdown-item">Logout</Link></li>
+                            </ul>
+                        </div>
+
                         {/* TODO: Change this link to the saved recipes page once built*/}
                         <Link to='/savedRecipes' className='header-right header-link saved-recipes'>
                         <img src={heart} alt='Saved Recipes' className='img-link saved-recipes'></img>
                         </Link>
+
+                        
                     </div>
                 </div>    
             </div>    
