f8852e4be52d02e23a2a41dc54bb04c321f259da
Fiona <fiona.beveridge@sky.uk>
2024-08-15 11:29:03 +0100

    populates profile page with user name and email, update details not yet implemented

--- site/src/components/Profile.jsx
+++ site/src/components/Profile.jsx
@@ -1,65 +1,154 @@
-import React from 'react';
-import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage } from 'mdb-react-ui-kit';
+import React, { useState, useEffect } from 'react';
+import '../css/Register.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import baseUrl from '../baseUrl';
 
-const PersonalProfile = () => {
-    return (
-        <div className="gradient-custom-2" style={{ backgroundColor: '#9de2ff' }}>
-          <MDBContainer className="py-5 h-100">
-            <MDBRow className="justify-content-center align-items-center h-100">
-              <MDBCol lg="9" xl="7">
-                <MDBCard>
-                  <div className="rounded-top text-white d-flex flex-row" style={{ backgroundColor: '#dc0119', height: '200px' }}>
-                    <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '150px' }}>
-                      <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
-                    alt="Avatar" className="my-5" style={{ width: '95px', marginLeft: '20px'}} fluid />
-                      <button outline color="dark" style={{height: '36px', overflow: 'visible'}}>
-                        Edit profile
-                      </button>
-                    </div>
-                    <div className="ms-3" style={{ marginTop: '130px' }}>
-                      <MDBCardText tag="h5">Welcome, User</MDBCardText>
-                      <MDBCardText></MDBCardText>
-                    </div>
-                  </div>
-                  <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
-                    <div className="d-flex justify-content-end text-center py-1">
-                      <div>
-                        <MDBCardText className="mb-1 h5">24</MDBCardText>
-                        <MDBCardText className="small text-muted mb-0">Saved Recipes</MDBCardText>
-                      </div>
-                    </div>
-                  </div>
-                  <MDBCardBody className="text-black p-4">
-                    <div className="d-flex justify-content-between align-items-center mb-4">
-                      <MDBCardText className="lead fw-normal mb-0 h5">My Saved Recipes</MDBCardText>
-                    </div>
-                    <MDBRow className="g-4">
-                      <MDBCol className="mb-2">
-                        <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(112).webp"
-                          alt="image 1" className="w-100 rounded-3" />
-                      </MDBCol>
-                      <MDBCol className="mb-2">
-                        <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(107).webp"
-                          alt="image 1" className="w-100 rounded-3" />
-                      </MDBCol>
-                    </MDBRow>
-                    <MDBRow className="g-4">
-                      <MDBCol className="mb-2">
-                        <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(108).webp"
-                          alt="image 1" className="w-100 rounded-3" />
-                      </MDBCol>
-                      <MDBCol className="mb-2">
-                        <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(114).webp"
-                          alt="image 1" className="w-100 rounded-3" />
-                      </MDBCol>
-                    </MDBRow>
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBCol>
-            </MDBRow>
-          </MDBContainer>
-        </div>
-      );
+function Profile({ setLoginChanged }) {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [errors, setErrors] = useState({});
+    const [userInfo, setUserInfo] = useState({});
+    
+    const nav = useNavigate();
+
+    const validateForm = () => {
+        const errors = {};
+        let charRange = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+        let firstNameIsValid = charRange.test(firstName) && firstName.length >= 2 && firstName.length <= 30;
+        let lastNameIsValid = charRange.test(lastName) && lastName.length >= 2 && lastName.length <= 30;
+
+        if (!firstName.trim()) {
+            errors.firstName = 'First name is required';
+        } else if (!firstNameIsValid) {
+            errors.firstName = 'Enter a valid first name';
+        }
+
+        if (!lastName.trim()) {
+            errors.lastName = 'Last name is required';
+        } else if (!lastNameIsValid) {
+            errors.lastName = 'Enter a valid last name';
+        }
+
+        if (!email.trim()) {
+            errors.email = 'Email is required';
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            errors.email = 'Email is invalid';
+        }
+
+        return errors;
+    };
+
+    useEffect(() => {
+      const getUserInfo = async () => {
+          let token = sessionStorage.getItem("token");
+          if (!token) {
+              nav("/login")
+              return
+          }
+
+          const url = process.env.REACT_APP_BASEURL + "/getUserInfo/" + token;
+          const userInfo = await axios.get(url).catch(err => console.log(err));
+          console.log(userInfo)
+          if (!userInfo) {
+            console.log("details failed to load")
+            return
+        }
+          setUserInfo(userInfo.data)
+      }
+      getUserInfo()
+  }, []);
+
+  
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const validationErrors = validateForm();
+        setErrors(validationErrors);
+
+        if (Object.keys(validationErrors).length === 0) {
+            // Handle form submission logic here
+
+            const createUserURL = process.env.REACT_APP_BASEURL + '/createUser'
+            const user = {
+                "firstname": firstName,
+                "surname": lastName,
+                "email": email,
+            };
+
+            await axios.post(createUserURL, user).then((res) => {
+                sessionStorage.setItem("token", res.data);
+                nav('/');
+                setLoginChanged(true);
+            }).catch((err) => {
+                setErrors(prevErrors => ({
+                    ...prevErrors,
+                    email: 'Email already exists'
+                }))
+            });
+        } else {
+            console.log('Form submission failed due to validation errors.');
+        }
     }
 
-export default PersonalProfile;
+    return (
+        <div className="register-container">
+            <form className="register-form d-flex flex-column p-4" onSubmit={handleSubmit}>
+                <div className="mb-3 w-100">
+                    <label htmlFor="firstName" className="form-label">First Name: </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={firstName}
+                        placeholder={userInfo.firstname}
+                        onChange={({ target }) => setFirstName(target.value)}
+                    />
+                    {errors.firstName && (
+                        <span className="error-message">
+                            {errors.firstName}
+                        </span>
+                    )}
+                </div>
+                <div className="mb-3 w-100">
+                    <label htmlFor="lastName" className="form-label">Last Name: </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={lastName}
+                        placeholder={userInfo.surname}
+                        onChange={({ target }) => setLastName(target.value)}
+                    />
+                    {errors.lastName && (
+                        <span className="error-message">
+                            {errors.lastName}
+                        </span>
+                    )}
+                </div>
+                <div className="mb-3 w-100">
+                    <label htmlFor="email" className="form-label">Email Address: </label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        value={email}
+                        placeholder={userInfo.email}
+                        onChange={({ target }) => setEmail(target.value)}
+                    />
+                    {errors.email && (
+                        <span className="error-message">
+                            {errors.email}
+                        </span>
+                    )}
+                </div>
+                
+                <button
+                    type="submit"
+                    className="btn btn-outline-dark w-50">
+                    Update Details
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default Profile;
